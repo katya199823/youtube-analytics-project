4e1e54b9cb8b1db4b8d5c93e815bc7b43293d2c1
Екатерина <Krinicyna1998@gmail.com>
2024-01-13 01:13:16 +0700

    change_channel.py

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,24 @@
+import os
+import json
+from googleapiclient.discovery import build
 
+api_key: str = os.getenv('API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь json в нормальном формате"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        printj(channel)
